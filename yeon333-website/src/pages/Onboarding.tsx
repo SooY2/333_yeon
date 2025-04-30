062754894cf4893da0d325e0618346f7b6600395
sooy <ssy67366736@gmail.com>
2025-04-30 18:32:01 +0900

    feat: lazy loading

--- yeon333-website/src/pages/Onboarding.tsx
+++ yeon333-website/src/pages/Onboarding.tsx
@@ -1,19 +1,30 @@
-import Footer from '../components/Common/Footer/Footer';
+import { lazy, Suspense } from 'react';
 import Header from '../components/Common/Header/Header';
-import Archive from '../components/Onboarding/ArchiveSection/Archive';
 import Banner from '../components/Onboarding/BannerSection/Banner';
-import Introduction from '../components/Onboarding/IntroductionSection/Introduction';
 import VirtualInstallation from '../components/Onboarding/VirtualInstallationSection/VirtualInstallation';
+
+// Lazy load components
+const Introduction = lazy(
+  () => import('../components/Onboarding/IntroductionSection/Introduction')
+);
+const Archive = lazy(
+  () => import('../components/Onboarding/ArchiveSection/Archive')
+);
+const Footer = lazy(() => import('../components/Common/Footer/Footer'));
 
 const Onboarding = () => {
   return (
     <>
       <Header />
       <Banner />
-      <Introduction />
+      <Suspense fallback={null}>
+        <Introduction />
+      </Suspense>
       <VirtualInstallation />
-      <Archive />
-      <Footer />
+      <Suspense fallback={null}>
+        <Archive />
+        <Footer />
+      </Suspense>
     </>
   );
 };
